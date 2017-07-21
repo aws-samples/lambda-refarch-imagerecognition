30385a4784eac6e38ec846bd830a8185e0304540
Aditya Pratap Singh <adisinghrajput@gmail.com>
2017-07-21 22:36:35 +0200

    Import only needed S3 service and not whole AWS reference lib

--- lambda-functions/thumbnail/index.js
+++ lambda-functions/thumbnail/index.js
@@ -1,14 +1,11 @@
 // dependencies
-const AWS = require('aws-sdk');
+const s3 = require('aws-sdk/clients/s3');
 const gm = require('gm').subClass({imageMagick: true}); // Enable ImageMagick integration.
 const util = require('util');
 
 // constants
 const MAX_WIDTH = 250;
 const MAX_HEIGHT = 250;
-
-// get reference to S3 client
-const s3 = new AWS.S3();
 
 /**
  * Generate a thumbnail of an image stored in s3 and store the thumbnail back in the same bucket under the "Thumbnail/" prefix
