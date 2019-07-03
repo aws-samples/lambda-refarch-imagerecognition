2317f564168f42cc7c2abe99dd7e21aad82f0fcd
Boaz Ziniman <boazz@amazon.com>
2019-07-03 18:20:00 +0100

    Updating Workshop Step 1

--- webapp/app/config.ts
+++ webapp/app/config.ts
@@ -8,10 +8,10 @@ export interface Config {
 }
 
 export const CONFIG: Config = {
-  DDBAlbumMetadataTable : "TO_BE_REPLACED",
-  CognitoIdentityPool : "TO_BE_REPLACED",
+  DDBAlbumMetadataTable : "photo-sharing-backend-AlbumMetadataDDBTable-10EV3LCQ5PT6M",
+  CognitoIdentityPool : "us-west-2:fa66b92c-27dd-4cdf-9cd4-5314d4190d57",
   Region : "us-west-2",   // might be replaced if you launched the template in a different region
-  DDBImageMetadataTable : "TO_BE_REPLACED",
-  S3PhotoRepoBucket : "TO_BE_REPLACED",
-  DescribeExecutionLambda : "TO_BE_REPLACED"
+  DDBImageMetadataTable : "photo-sharing-backend-ImageMetadataDDBTable-JAVKSMXN0ETI",
+  S3PhotoRepoBucket : "photo-sharing-backend-photorepos3bucket-7inrc7x5qvdf",
+  DescribeExecutionLambda : "photo-sharing-backend-DescribeExecutionFunction-2011F3H38HLI"
 };
