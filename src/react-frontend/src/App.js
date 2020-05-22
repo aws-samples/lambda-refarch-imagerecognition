c3fdabeae6b76b733bdf0939010c3ae542dc97af
Angela W <angelaw@amazon.com>
2020-05-22 16:56:05 -0400

    fix premature appsync call before login

--- src/react-frontend/src/App.js
+++ src/react-frontend/src/App.js
@@ -4,6 +4,7 @@
 //  or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
 import React from 'react';
+import {Auth} from 'aws-amplify';
 
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
@@ -65,7 +66,7 @@ function App() {
           <Grid.Column>
 
             <Route path="/" exact component={NewAlbum}/>
-            <Route path="/" exact component={AlbumList}/>
+            <Route path="/" exact component={()=> !Auth.currentAuthenticatedUser()? null : <AlbumList/>}/>
             <Route
               path="/albums/:albumId"
               render={props => <AlbumDetails id={props.match.params.albumId}/>}/>
